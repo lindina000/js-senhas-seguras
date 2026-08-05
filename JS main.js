62dfb03e5b9b93aecc18607c238780a34e08cb78
lindina000 <kemily.santos.luz@escola.pr.gov.br>
2026-08-04 21:25:53 -0300

    aula 04/08

---
+++ JS main.js
@@ -0,0 +1,21 @@
+consst numeroSenha = document.querySelector('.paramento-senha__texto');
+const botoes = document. querySelectorAll('.paramentro-senha__botao');
+
+let tamanhoSenha = 12;
+
+botoes[0].onclick = diminui Tamanho;
+botoes[1].onclick = aumenta Tamanho;
+
+function diminue Tamanho() {
+    if (tamanhoSenha > 1) {
+    tamanhoSenha = tamanhoSenha - 1;
+}
+numeroSenha.textcontent = tamanhoSenha  
+}
+        
+    function aumentaTamanho() {
+        if (tamanhoSenha > 20) {
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+numeroSenha.textcontent = tamanhoSenha
+}
